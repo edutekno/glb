73e7fd056c4ff03d7b2f0ed752c0bc29b181454e
edutekno <54890481+edutekno@users.noreply.github.com>
2020-10-25 12:39:44 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+$glb=””;$obj=””;$mtl=””;
+echo $glb=$_GET[‘glb’];
+$obj=$_GET[‘obj’];
+$mtl=$_GET[‘mtl’];
+
+?>
+
+ <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+  </head>
+  <body>
+<a-scene>
+<a-assets>
+<?php if (strlen($glb)> 4){?>
+<a-asset-item id=”glbmodel” response-type=”arraybuffer” src=”<?php echo $glb;?>”></a-asset-item>
+<?php }?>
+</a-assets>
+
+<a-entity gltf-model=”#glbmodel” position=”-1 1 -2″ scale=”0.5 0.5 0.5″ animation-mixer=”clip:mixamo.com; loop:2; timeScale: 2; crossFadeDuration: 1″></a-entity>
+
+</a-scene>
+    
+    
+<a-entity position="0 0 1">
+<a-camera user-height="1" >
+<a-cursor fuse=true fuseTimeout="2000" ></a-cursor>
+</a-camera></a-entity>
+
+    
+  </body>
+</html>
